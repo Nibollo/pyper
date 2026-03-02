4cff4baa8ceffea21a1b6dfecfa1d7ba93661cdd
Celso Arce <51603197+Nibollo@users.noreply.github.com>
2026-03-02 12:09:32 -0300

    feat: integrate Google Tag Manager and update configs for Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import AnalyticsTracker from "@/components/AnalyticsTracker";
 import { CartProvider } from "@/context/CartContext";
 import { ConfigProvider } from "@/context/ConfigContext";
 import { supabase } from "@/lib/supabase";
+import Script from "next/script";
 
 export async function generateMetadata(): Promise<Metadata> {
   try {
@@ -43,6 +44,31 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WCMLWQ98"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WCMLWQ98');
+            `,
+          }}
+        />
+
         <ConfigProvider>
           <CartProvider>
             <Navbar />
